291454974cfe59c5902476a0f176aa47221eaeec
Areeba303 <w46359486@gmail.com>
2024-10-30 05:52:27 -0700

    Add files via upload

---
+++ assignment 1.r
@@ -0,0 +1,43 @@
+#Question 1
+number1=2
+number2=4
+number3=6
+
+#calculate sum
+sum=number1+number2+number3 #sum
+print("sum:",sum)
+
+#calculate difference
+difference=number1-number2-number3 #difference
+print("difference:",difference)
+
+#calculate product
+product=number1*number2*number3 #product
+print("product:",product)
+
+#calculate average
+average=(number1+number2+number3)/3 #average
+print("average:",average)
+
+#area of triangle
+breadth=22
+height=25
+area=0.5*breadth*height
+print("triangle area:",area)
+
+#area of square
+side=4 
+area=side*side
+print("square area:",area)
+
+#area of circle
+pi=3.14
+radius=9
+area=pi*radius*radius
+print("circle area:",area)
+
+#rectangle area
+length=5
+width=7
+area=length*width
+print("rectangle area:",area)
